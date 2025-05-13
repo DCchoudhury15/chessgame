6986f1b3e1c1cbb2fc48a10b027db1f7e88eb158
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-05-13 16:34:49 +0530

    Initial commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",    // or "./src/**/*.{html,js,ejs}"
+    "./public/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
